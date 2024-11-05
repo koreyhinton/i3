8817e240536a39e0473b1fb4550adeadc7b2902d
Kasey Hinton <kaseyjameshinton@gmail.com>
2024-11-05 00:01:27 -0700

    Add some services and git configuration

--- install.sh
+++ install.sh
@@ -13,7 +13,21 @@ xbps-install -Su \
     i3 \
     i3status \
     dmenu \
-    nvidia
+    nvidia \
+    dbus \
+    lightdm \
+    lightdm-gtk3-greeter
+
+# Set up dbus as service 
+sudo ln -s /etc/sv/dbus /var/service/dbus 
+
+# set up lightdm as a service
+sudo ln -s /etc/sv/lightdm /var/service/lightdm
+
+# Set up git info
+git config --global user.name "kaseyhinton"
+git config --global user.email "20712428+kaseyhinton@users.noreply.github.com"
+
 
 echo "exec i3" >> ~/.xinitrc
 
