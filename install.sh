b4a3fffb8861db6c8368896684475131bb9036ef
Korey Hinton <2801399+koreyhinton@users.noreply.github.com>
2024-11-16 13:49:06 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,34 +1,11 @@
 #!/bin/bash
 
-xbps-install -Su \
-    void-repo-nonfree \
-    void-repo-multilib-nonfree \
-    void-repo-multilib \
-    neofetch \
-    vpm \
-    git \
-    curl \
-    alacritty \
+apt-get update -y
+yes | apt-get install \
     xorg \
     i3 \
     i3status \
-    dmenu \
-    nvidia \
-    dbus \
-    lightdm \
-    lightdm-gtk3-greeter \
-    polybar \
-    feh
-
-# Set up dbus as service 
-sudo ln -s /etc/sv/dbus /var/service/dbus 
-
-# set up lightdm as a service
-sudo ln -s /etc/sv/lightdm /var/service/lightdm
-
-# Set up git info
-git config --global user.name "kaseyhinton"
-git config --global user.email "20712428+kaseyhinton@users.noreply.github.com"
+    dmenu
 
 echo "exec i3" >> ~/.xinitrc
 
